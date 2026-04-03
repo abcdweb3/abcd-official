78cc54db16a4d2d1345835a096cfc100745bec11
abcdweb3 <abcd.web3@gmail.com>
2026-04-03 18:56:50 +0800

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'abcd-purple': '#6366f1',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
