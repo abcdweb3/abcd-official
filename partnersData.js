f2f86e8f2ceeb8888567f3505616b85518f35898
abcdweb3 <abcd.web3@gmail.com>
2026-04-04 00:05:20 +0800

    Create partnersData.js

---
+++ partnersData.js
@@ -0,0 +1,8 @@
+// partnersData.js
+export const partners = [
+  { name: "O2Pay", logo: "/logos/o2pay.png", type: "Platinum" },
+  { name: "CertiK", logo: "/logos/certik.png", type: "Security" },
+  { name: "Pudgy Malaysia", logo: "/logos/pudgy.png", type: "Community" },
+  { name: "MYBW", logo: "/logos/mybw.png", type: "Event" },
+  // 以后直接在这里复制粘贴
+];
